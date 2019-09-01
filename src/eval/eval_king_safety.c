70935c46d53fcfa001125478db084c5131a29aa2
James Swafford <j.f.swafford@gmail.com>
2019-09-01 17:58:19 -0400

    WIP - king safety middle of board

--- src/eval/eval_king_safety.c
+++ src/eval/eval_king_safety.c
@@ -1,4 +1,4 @@
-#include <prophet/parameters.h>
+#include <prophet/bitmap.h>
 
 #include "eval_internal.h"
 
@@ -11,17 +11,58 @@
  *
  * \return a score of the king safety for the player specified.
  */
-int32_t eval_king_safety(const position_t* UNUSED(pos), bool wtm)
+int32_t eval_king_safety(const position_t* pos, bool wtm)
 {
+    int32_t score = 0;
 
     if (wtm)
     {
+        file_t file_king = get_file(pos->white_king);
 
+        /* king side */
+        if (file_king > FILE_E)
+        {
+
+        }
+        /* queen side */
+        else if (file_king < FILE_D)
+        {
+
+        }
+        /* middle */
+        else
+        {
+            if (! ((pos->white_pawns | pos->black_pawns) & 
+                sq_to_file_bitmap(pos->white_king))) 
+            {
+                score += king_safety_middle_open_file;
+            }
+        }
     }
     else /* black to move */
     {
+        file_t file_king = get_file(pos->black_king);
 
+        /* king side */
+        if (file_king > FILE_E)
+        {
+
+        }
+        /* queen side */
+        else if (file_king < FILE_D)
+        {
+
+        }
+        /* middle */
+        else
+        {
+            if (! ((pos->white_pawns | pos->black_pawns) & 
+                sq_to_file_bitmap(pos->black_king))) 
+            {
+                score += king_safety_middle_open_file;
+            }
+        }
     }
 
-    return 0;
+    return score;
 }
--- test/eval/test_king_safety.cpp
+++ test/eval/test_king_safety.cpp
@@ -2,7 +2,7 @@
 
 #include "../../src/eval/eval_internal.h"
 
-TEST(eval_test, DISABLED_king_safety_middle_files)
+TEST(eval_test, king_safety_middle_files)
 {
     position_t pos;
 
@@ -13,20 +13,23 @@ TEST(eval_test, DISABLED_king_safety_middle_files)
     EXPECT_EQ(0, eval_king_safety(&pos, true));
     EXPECT_EQ(0, eval_king_safety(&pos, false));
 
-    // open file for both, so still no penalty
+    // open file for both
     ASSERT_TRUE(set_pos(&pos,
         "rnbqkbnr/pppp1ppp/8/8/8/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1"));
 
-    EXPECT_EQ(0, eval_king_safety(&pos, true));
-    EXPECT_EQ(0, eval_king_safety(&pos, false));
+    EXPECT_EQ(king_safety_middle_open_file, 
+        eval_king_safety(&pos, true));
+    EXPECT_EQ(king_safety_middle_open_file, 
+        eval_king_safety(&pos, false));
 
     // remove both queens.  open e file.  put black king on d8
+    // white should be penalized but black is not.
     ASSERT_TRUE(set_pos(&pos,
         "rnbk1bnr/pppp1ppp/8/8/8/8/PPPP1PPP/RNB1KBNR b KQ - 0 1"));
 
-    EXPECT_EQ(0, eval_king_safety(&pos, true));
     EXPECT_EQ(king_safety_middle_open_file, 
-        eval_king_safety(&pos, false));
+        eval_king_safety(&pos, true));
+    EXPECT_EQ(0, eval_king_safety(&pos, false));
 
 }
 
