967e3c33ca389911cb809f68f805940df7824f19
James Swafford <j.f.swafford@gmail.com>
2019-09-01 19:28:33 -0400

    completed king safety

--- src/eval/eval_king_safety.c
+++ src/eval/eval_king_safety.c
@@ -22,12 +22,110 @@ int32_t eval_king_safety(const position_t* pos, bool wtm)
         /* king side */
         if (file_king > FILE_E)
         {
+            /* test the F pawn */
+            if (pos->piece[F2] != PAWN)
+            {
+                if (pos->piece[F3] == PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[F4] == PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
 
+            /* test the G pawn */
+            if (pos->piece[G2] != PAWN)
+            {
+                if (pos->piece[G3] == PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[G4] == PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
+
+            /* test the H pawn */
+            if (pos->piece[H2] != PAWN)
+            {
+                if (pos->piece[H3] == PAWN)
+                {
+                    score += king_safety_pawn_one_away / 2;
+                }
+                else if (pos->piece[H4] == PAWN)
+                {
+                    score += king_safety_pawn_two_away / 2;
+                }
+                else
+                {
+                    score += king_safety_pawn_far_away / 2;
+                }
+            }
         }
         /* queen side */
         else if (file_king < FILE_D)
         {
+            /* test the C pawn */
+            if (pos->piece[C2] != PAWN)
+            {
+                if (pos->piece[C3] == PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[C4] == PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
 
+            /* test the B pawn */
+            if (pos->piece[B2] != PAWN)
+            {
+                if (pos->piece[B3] == PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[B4] == PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
+
+            /* test the A pawn */
+            if (pos->piece[A2] != PAWN)
+            {
+                if (pos->piece[A3] == PAWN)
+                {
+                    score += king_safety_pawn_one_away / 2;
+                }
+                else if (pos->piece[A4] == PAWN)
+                {
+                    score += king_safety_pawn_two_away / 2;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away / 2;
+                }
+            }
         }
         /* middle */
         else
@@ -46,12 +144,110 @@ int32_t eval_king_safety(const position_t* pos, bool wtm)
         /* king side */
         if (file_king > FILE_E)
         {
+            /* test the F pawn */
+            if (pos->piece[F7] != -PAWN)
+            {
+                if (pos->piece[F6] == -PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[F5] == -PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
 
+            /* test the G pawn */
+            if (pos->piece[G7] != -PAWN)
+            {
+                if (pos->piece[G6] == -PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[G5] == -PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
+
+            /* test the H pawn */
+            if (pos->piece[H7] != -PAWN)
+            {
+                if (pos->piece[H6] == -PAWN)
+                {
+                    score += king_safety_pawn_one_away / 2;
+                }
+                else if (pos->piece[H5] == -PAWN)
+                {
+                    score += king_safety_pawn_two_away / 2;
+                }
+                else
+                {
+                    score += king_safety_pawn_far_away / 2;
+                }
+            }
         }
         /* queen side */
         else if (file_king < FILE_D)
         {
+            /* test the C pawn */
+            if (pos->piece[C7] != -PAWN)
+            {
+                if (pos->piece[C6] == -PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[C5] == -PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
 
+            /* test the B pawn */
+            if (pos->piece[B7] != -PAWN)
+            {
+                if (pos->piece[B6] == -PAWN)
+                {
+                    score += king_safety_pawn_one_away;
+                }
+                else if (pos->piece[B5] == -PAWN)
+                {
+                    score += king_safety_pawn_two_away;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away;
+                }
+            }
+
+            /* test the A pawn */
+            if (pos->piece[A7] != -PAWN)
+            {
+                if (pos->piece[A6] == -PAWN)
+                {
+                    score += king_safety_pawn_one_away / 2;
+                }
+                else if (pos->piece[A5] == -PAWN)
+                {
+                    score += king_safety_pawn_two_away / 2;
+                }
+                else 
+                {
+                    score += king_safety_pawn_far_away / 2;
+                }
+            }
         }
         /* middle */
         else
--- test/eval/test_king_safety.cpp
+++ test/eval/test_king_safety.cpp
@@ -33,3 +33,59 @@ TEST(eval_test, king_safety_middle_files)
 
 }
 
+TEST(eval_test, king_safety_king_side)
+{
+    position_t pos;
+
+    // no pawns advanced
+    ASSERT_TRUE(set_pos(&pos, 
+        "rnbq1rk1/pppppppp/8/8/8/8/PPPPPPPP/RNBQ1RK1 w - - 0 1"));
+
+    EXPECT_EQ(0, eval_king_safety(&pos, true));
+    EXPECT_EQ(0, eval_king_safety(&pos, false));
+
+    // white pawn on F3
+    ASSERT_TRUE(set_pos(&pos, 
+        "rnbq1rk1/pppppppp/8/8/8/5P2/PPPPP1PP/RNBQ1RK1 w - - 0 1"));
+
+    EXPECT_EQ(king_safety_pawn_one_away, 
+        eval_king_safety(&pos, true));
+    EXPECT_EQ(0, eval_king_safety(&pos, false));
+
+    // white pawn on G4
+    ASSERT_TRUE(set_pos(&pos, 
+        "rnbq1rk1/pppppppp/8/8/6P1/8/PPPPPP1P/RNBQ1RK1 w - - 0 1"));
+
+    EXPECT_EQ(king_safety_pawn_two_away, 
+        eval_king_safety(&pos, true));
+    EXPECT_EQ(0, eval_king_safety(&pos, false));
+}
+
+TEST(eval_test, king_safety_queen_side)
+{
+    position_t pos;
+
+    // pawn on C3
+    ASSERT_TRUE(set_pos(&pos, 
+        "1krq1bnr/pppppppp/8/8/8/2P5/PP1PPPPP/1KRQ1BNR w - - 0 1"));
+
+    EXPECT_EQ(king_safety_pawn_one_away, 
+        eval_king_safety(&pos, true));
+    EXPECT_EQ(0, eval_king_safety(&pos, false));
+
+    // white pawn on B4
+    ASSERT_TRUE(set_pos(&pos, 
+        "1krq1bnr/pppppppp/8/8/1P6/8/P1PPPPPP/1KRQ1BNR w - - 0 1"));
+
+    EXPECT_EQ(king_safety_pawn_two_away, 
+        eval_king_safety(&pos, true));
+    EXPECT_EQ(0, eval_king_safety(&pos, false));
+
+    // black pawn on A4
+    ASSERT_TRUE(set_pos(&pos, 
+        "1krq1bnr/1ppppppp/8/8/p7/8/PPPPPPPP/1KRQ1BNR b - - 0 1"));
+
+    EXPECT_EQ(0, eval_king_safety(&pos, true));
+    EXPECT_EQ(king_safety_pawn_far_away/2, 
+        eval_king_safety(&pos, false));
+}
