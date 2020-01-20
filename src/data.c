54eb423e5492b41e0f58f183f30ca289aa28b0c4
James Swafford <j.f.swafford@gmail.com>
2020-01-20 15:57:33 -0500

    added global move stack

--- src/data.c
+++ src/data.c
@@ -5,6 +5,9 @@
 /* the game position, or global position */
 position_t gpos;
 
+/* move stack */
+move_t gmoves[MAX_PLY * MAX_MOVES_PER_PLY];
+
 /* undo information for the game position*/
 undo_t gundos[MAX_HALF_MOVES_PER_GAME];
 
