8e252c914cca5021a279f8abc90aaa4b3e50dc74
James Swafford <j.f.swafford@gmail.com>
2020-06-06 09:28:16 -0400

    more timing tweaks

--- test/command/xboard/test_xboard_time.cpp
+++ test/command/xboard/test_xboard_time.cpp
@@ -32,7 +32,11 @@ TEST(xboard_test, xboard_time_fixed_time_per_move)
 
     ASSERT_EQ(0, xboard_time("time 1000")); // 10 seconds in centiseconds
     EXPECT_EQ(10000U, time_remaining_millis);
-    EXPECT_EQ(9950U, max_time_ms);
+    EXPECT_EQ(9900U, max_time_ms);
+
+    ASSERT_EQ(0, xboard_time("time 6")); // 60 milliseconds
+    EXPECT_EQ(60U, time_remaining_millis);
+    EXPECT_EQ(30U, max_time_ms);
 
     ASSERT_EQ(0, xboard_time("time 4")); // 40 milliseconds
     EXPECT_EQ(40U, time_remaining_millis);
@@ -50,23 +54,23 @@ TEST(xboard_test, xboard_time_not_fixed_time_per_move)
     time_control_increment = 0;
     ASSERT_EQ(0, xboard_time("time 1000")); // 10 seconds in centiseconds
     EXPECT_EQ(10000U, time_remaining_millis);
-    EXPECT_EQ(398U, max_time_ms);
+    EXPECT_EQ(400U, max_time_ms);
 
     ASSERT_EQ(0, xboard_time("time 25"));
     EXPECT_EQ(250U, time_remaining_millis);
-    EXPECT_EQ(8U, max_time_ms);
+    EXPECT_EQ(10U, max_time_ms);
 
     time_control_increment = 12;
     ASSERT_EQ(0, xboard_time("time 25"));
     EXPECT_EQ(250U, time_remaining_millis);
-    EXPECT_EQ(11408U, max_time_ms);
+    EXPECT_EQ(11910U, max_time_ms);
 
     ASSERT_EQ(0, xboard_time("time -2"));
     EXPECT_EQ(0U, time_remaining_millis);
-    EXPECT_EQ(11401U, max_time_ms);
+    EXPECT_EQ(11900U, max_time_ms);
 
     time_control_increment = 0.5F;
     ASSERT_EQ(0, xboard_time("time 1000"));
     EXPECT_EQ(10000U, time_remaining_millis);
-    EXPECT_EQ(873U, max_time_ms);
+    EXPECT_EQ(800U, max_time_ms);
 }
