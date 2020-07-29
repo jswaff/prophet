54b8146e39419ffa38635fd22ff6b91a442badd5
James Swafford <j.f.swafford@gmail.com>
2020-07-29 17:03:03 -0400

    zugzwang

--- src/search/search.c
+++ src/search/search.c
@@ -148,7 +148,7 @@ static int32_t search_helper(position_t* pos, move_line_t* parent_pv,
         }
 
         /* null move */
-        if (!first && !incheck && null_move_ok && depth >= 3)
+        if (!first && !incheck && null_move_ok && depth >= 3 && !zugzwang(pos))
         {
             square_t ep_sq = apply_null_move(pos);
             int null_depth = depth - 4; /* R=3 */
