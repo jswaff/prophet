0ed9481cb9c6b17c7508a3a14c6de47c0dbe68b0
James Swafford <j.f.swafford@gmail.com>
2021-09-08 20:39:36 -0400

    whitespace change

--- test/eval/test_eval_draw.cpp
+++ test/eval/test_eval_draw.cpp
@@ -8,62 +8,62 @@ static void test_with_symmetry(const char* fen, bool draw);
 
 TEST(eval_test, eval_draw_kk)
 {
-	test_with_symmetry("k7/8/8/8/8/8/8/K7 w - -", true);
+    test_with_symmetry("k7/8/8/8/8/8/8/K7 w - -", true);
 }
 
 TEST(eval_test, eval_draw_knk)
 {
-	test_with_symmetry("kn6/8/8/8/8/8/8/K7 w - -", true);
+    test_with_symmetry("kn6/8/8/8/8/8/8/K7 w - -", true);
 }
 
 TEST(eval_test, eval_draw_kbk)
 {
-	test_with_symmetry("kb6/8/8/8/8/8/8/K7 w - -", true);
+    test_with_symmetry("kb6/8/8/8/8/8/8/K7 w - -", true);
 }
 
 TEST(eval_test, eval_draw_kpk)
 {
-	test_with_symmetry("k7/p7/8/8/8/8/8/K7 w - -", false);
+    test_with_symmetry("k7/p7/8/8/8/8/8/K7 w - -", false);
 }
 
 TEST(eval_test, eval_draw_kqk)
 {
-	test_with_symmetry("kq6/8/8/8/8/8/8/K7 w - -", false);
+    test_with_symmetry("kq6/8/8/8/8/8/8/K7 w - -", false);
 }
 
 TEST(eval_test, eval_draw_krk)
 {
-	test_with_symmetry("kr6/8/8/8/8/8/8/K7 w - -", false);
+    test_with_symmetry("kr6/8/8/8/8/8/8/K7 w - -", false);
 }
 
 TEST(eval_test, eval_draw_kbbk)
 {
-	test_with_symmetry("kbb5/8/8/8/8/8/8/K7 w - -", false);
+    test_with_symmetry("kbb5/8/8/8/8/8/8/K7 w - -", false);
 }
 
 TEST(eval_test, eval_draw_kbnk)
 {
-	test_with_symmetry("kbn5/8/8/8/8/8/8/K7 w - -", false);
+    test_with_symmetry("kbn5/8/8/8/8/8/8/K7 w - -", false);
 }
 
 TEST(eval_test, eval_draw_knnk)
 {
-	test_with_symmetry("knn5/8/8/8/8/8/8/K7 w - -", true);
+    test_with_symmetry("knn5/8/8/8/8/8/8/K7 w - -", true);
 }
 
 TEST(eval_test, eval_draw_kbkb)
 {
-	test_with_symmetry("kb6/8/8/8/8/8/8/KB6 w - -", true);
+    test_with_symmetry("kb6/8/8/8/8/8/8/KB6 w - -", true);
 }
 
 TEST(eval_test, eval_draw_kbkn)
 {
-	test_with_symmetry("kb6/8/8/8/8/8/8/KN6 w - -", true);
+    test_with_symmetry("kb6/8/8/8/8/8/8/KN6 w - -", true);
 }
 
 TEST(eval_test, eval_draw_knkn)
 {
-	test_with_symmetry("kn6/8/8/8/8/8/8/KN6 w - -", true);
+    test_with_symmetry("kn6/8/8/8/8/8/8/KN6 w - -", true);
 }
 
 static void test_with_symmetry(const char* fen, bool draw) 
