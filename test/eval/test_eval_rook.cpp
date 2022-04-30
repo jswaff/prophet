b8f1a91afe986ad86f511c07eac80f1dc5d3559e
James Swafford <j.f.swafford@gmail.com>
2022-04-30 17:48:22 -0400

    rook tests

--- test/eval/test_eval_rook.cpp
+++ test/eval/test_eval_rook.cpp
@@ -26,9 +26,11 @@ TEST(eval_test, eval_rook_7th_king_back_rank)
 {
     position_t pos;
     ASSERT_TRUE(set_pos(&pos, "7k/2Q2R2/8/8/8/8/r7/7K w - - 0 1"));
-    
-    // EXPECT_EQ(rook_pst[F7] + major_on_7th + rook_open_file,
-    //     eval_rook(&pos, F7, false));
+
+    int32_t mg = 0; int32_t eg = 0;
+    eval_rook(&pos, F7, &mg, &eg);
+    EXPECT_EQ(rook_pst[F7] + major_on_7th + rook_open_file, mg);
+    EXPECT_EQ(rook_endgame_pst[F7] + major_on_7th + rook_open_file, eg);
 }
 
 
@@ -37,8 +39,10 @@ TEST(eval_test, eval_rook_open_file)
     position_t pos;
     ASSERT_TRUE(set_pos(&pos, "3r3k/8/8/8/8/8/8/7K b - - 0 1"));
 
-    // EXPECT_EQ(rook_pst[D1] + rook_open_file,
-    //     eval_rook(&pos, D8, false));
+    int32_t mg = 0; int32_t eg = 0;
+    eval_rook(&pos, D1, &mg, &eg);
+    EXPECT_EQ(-rook_pst[D8] - rook_open_file, mg);
+    EXPECT_EQ(-rook_endgame_pst[D8] - rook_open_file, eg);
 }
 
 TEST(eval_test, eval_rook_half_open_file)
@@ -48,11 +52,16 @@ TEST(eval_test, eval_rook_half_open_file)
     /* friendly pawn but no enemy -- not half open (or open) */
     ASSERT_TRUE(set_pos(&pos, "8/2P5/8/2R5/K7/8/7k/8 w - - 0 1"));
 
-//    EXPECT_EQ(rook_pst[C5], eval_rook(&pos, C5, false));
+    int32_t mg = 0; int32_t eg = 0;
+    eval_rook(&pos, C5, &mg, &eg);
+    EXPECT_EQ(rook_pst[C5] , mg);
+    EXPECT_EQ(rook_endgame_pst[C5], eg);
 
     /* enemy pawn on C makes it half open */
     ASSERT_TRUE(set_pos(&pos, "8/2p5/8/2R5/K7/8/7k/8 w - - 0 1"));
 
-    // EXPECT_EQ(rook_pst[C5] + rook_half_open_file, 
-    //     eval_rook(&pos, C5, false));
+    int32_t mg2 = 0; int32_t eg2 = 0;
+    eval_rook(&pos, C5, &mg2, &eg2);
+    EXPECT_EQ(rook_pst[C5] + rook_half_open_file , mg2);
+    EXPECT_EQ(rook_endgame_pst[C5] + rook_half_open_file, eg2);
 }
