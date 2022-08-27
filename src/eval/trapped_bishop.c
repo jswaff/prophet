19510072164679caac51afde51d70d694e307b3f
James Swafford <j.f.swafford@gmail.com>
2022-08-27 11:25:43 -0400

    white space

--- src/eval/trapped_bishop.c
+++ src/eval/trapped_bishop.c
@@ -20,27 +20,26 @@ bool trapped_bishop(const position_t* pos, square_t sq, bool is_white)
 
     if (is_white)
     {
-    	if (sq == A7)
-    	{
-    		return pos->piece[B6]==-PAWN && pos->piece[C7]==-PAWN;
-    	}
-    	else if (sq == H7)
-    	{
-    		return pos->piece[F7]==-PAWN && pos->piece[G6]==-PAWN;	
-    	}
+        if (sq == A7)
+        {
+            return pos->piece[B6]==-PAWN && pos->piece[C7]==-PAWN;
+        }
+        else if (sq == H7)
+        {
+           return pos->piece[F7]==-PAWN && pos->piece[G6]==-PAWN;	
+        }
     }
     else
     {
-    	if (sq == A2)
-    	{
-    		return pos->piece[B3]==PAWN && pos->piece[C2]==PAWN;
-    	}
-    	else if (sq == H2)
-    	{
-    		return pos->piece[F2]==PAWN && pos->piece[G3]==PAWN;	
-    	}
+        if (sq == A2)
+        {
+            return pos->piece[B3]==PAWN && pos->piece[C2]==PAWN;
+        }
+        else if (sq == H2)
+        {
+            return pos->piece[F2]==PAWN && pos->piece[G3]==PAWN;	
+        }
     }
 
     return false;
 }
-
