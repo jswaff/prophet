ce1040cfc63fb6ae46345bbe0f6a99b06376062a
James Swafford <j.f.swafford@gmail.com>
2025-04-10 09:26:51 -0400

    use intrinsics for calculating L2

--- src/nn/nn_eval.c
+++ src/nn/nn_eval.c
@@ -32,19 +32,12 @@ int nn_eval(const position_t* pos, const neural_network_t* nn) {
 
     /* calculate layer 2 */
     int32_t L2[NN_SIZE_L2];
-    for (int i=0;i<NN_SIZE_L2;i++) {
-        int32_t sum = nn->B1[i];
-        for (int j=0;j<NN_SIZE_L1*2;j++) {
-            sum += nn->W1[i * (NN_SIZE_L1 * 2) + j]  * L1[j];
-        }
-        L2[i] = sum;
-    }
 
-    /* with intrinsics */
+#ifdef USE_AVX
     const __m256i one = _mm256_set1_epi16(1);
     for (int i=0;i<NN_SIZE_L2;i++) {
         __m256i sum0 = _mm256_setzero_si256();
-        for (int j=0;j<NN_SIZE_L1;j++) {
+        for (int j=0;j<NN_SIZE_L1*2;j+=32) {
             const __m256i inp = _mm256_loadu_si256((__m256i*) &L1[j]);
             const __m256i wei = _mm256_loadu_si256((__m256i*) &nn->W1[i * (NN_SIZE_L1*2) + j]);            
             const __m256i dot = _mm256_madd_epi16(_mm256_maddubs_epi16(inp, wei), one);
@@ -56,9 +49,17 @@ int nn_eval(const position_t* pos, const neural_network_t* nn) {
         __m128i sum128 = _mm_add_epi32(sum128lo, sum128hi); 
         sum128 = _mm_add_epi32(sum128, _mm_shuffle_epi32(sum128, _MM_PERM_ABCD));
         sum128 = _mm_add_epi32(sum128, _mm_shuffle_epi32(sum128, _MM_PERM_CDAB));
-        assert(_mm_cvtsi128_si32(sum128) + nn->B1[i] == L2[i]);
-        //L2[i] = _mm_cvtsi128_si32(sum128) + nn->B1[i];
+        L2[i] = _mm_cvtsi128_si32(sum128) + nn->B1[i];
     }
+#else /* without AVX intrinsics */
+    for (int i=0;i<NN_SIZE_L2;i++) {
+        int32_t sum = nn->B1[i];
+        for (int j=0;j<NN_SIZE_L1*2;j++) {
+            sum += nn->W1[i * (NN_SIZE_L1 * 2) + j]  * L1[j];
+        }
+        L2[i] = sum;
+    }
+#endif /* USE_AVX */
 
     /* translate to predicted score */
     float y = ((float)L2[0]) / (SCALE * SCALE);
