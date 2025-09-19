818537bd88c35ba27bb6c3b1aa0c69666de82d4e
James Swafford <j.f.swafford@gmail.com>
2025-09-19 08:00:40 -0400

    bump version to 5.2-dev

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     }
 
     /* welcome message, indicates initialization was successful */
-    plog("Hello!  This is the Prophet Chess Engine, version 5.1.\n\n");
+    plog("Hello!  This is the Prophet Chess Engine, version 5.2-dev.\n\n");
 
     retval = command_loop();
 
