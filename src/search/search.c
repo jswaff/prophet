6d62d8a271c83b479b0db4b6d0c8f7e9c548cb13
James Swafford <j.f.swafford@gmail.com>
2026-07-03 08:38:21 -0400

    hoist eval out of loop

--- src/search/search.c
+++ src/search/search.c
@@ -169,6 +169,7 @@ static int32_t search_helper(position_t *pos, move_line_t *parent_pv, bool first
     move_t best_move = NO_MOVE;
     move_t *mp;
     undo_t *uptr = undo_stack + pos->move_counter;
+    int32_t mat = (!incheck && depth < 3) ? eval(pos, true, false) : 0;
     while (next(pos, &mp, &mo_dto)){
         assert(get_move_score(*mp)==0);
 
@@ -177,7 +178,6 @@ static int32_t search_helper(position_t *pos, move_line_t *parent_pv, bool first
             alpha > (-CHECKMATE+500) && beta < (CHECKMATE-500) && 
             get_promopiece(*mp)==NO_PIECE && *mp != killer1[ply] && *mp != killer2[ply])
         {
-            int32_t mat = eval(pos, true, false);
             int32_t mat_gain = see_eval_piece(get_captured_piece(*mp));
             const int32_t futil_margin = depth==1 ? (see_pawn_val * 2) : see_rook_val;
             if (mat + mat_gain + futil_margin <= alpha) {
