2e3183ff4de9a6f9f878f22a312c784c18785c7d
James Swafford <j.f.swafford@gmail.com>
2026-08-22 15:38:31 -0400

    update nn.  remove noisy tests that add no value

--- test/nn/test_load_network.cpp
+++ test/nn/test_load_network.cpp
@@ -13,10 +13,6 @@ TEST(nn_test, nn_load_network)
 {
     std::string nn_path = test_resource_path("nn.txt");
     ASSERT_EQ(0, load_neural_network(nn_path.c_str()));
-
-    EXPECT_EQ(0, neural_network.W0[0]);
-    EXPECT_EQ(1, neural_network.B1[0]);
-    EXPECT_EQ(1, neural_network.B1[1]);
     EXPECT_TRUE(use_neural_network);
 
     // cleanup
